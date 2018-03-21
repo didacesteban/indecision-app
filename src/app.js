bf822e843f941ad52f35c26ab9775a41f94dd39f
Didac Esteban <didacesteban@MacBook-Pro-de-Daniel.local>
2018-03-21 09:33:58 +0100

    updating project

--- src/app.js
+++ src/app.js
@@ -27,19 +27,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  removeAll() {
+    alert('remove all');
+  }
   render() {
     return (
       <div>
+        <button onClick={this.removeAll}>Remove all</button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option}/>)
         }
@@ -59,10 +66,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      alert(option);
+    }
+  }
   render() {
     return (
       <div>
-        <button>Add option</button>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
       </div>
     );
   }
