51db4bdb75aeb1a0b5b748c04244277c42df84af
Didac Esteban Andres <didacesteban@gmail.com>
2018-03-23 19:34:10 +0100

    adding state

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -105,16 +105,19 @@ var Action = function (_React$Component3) {
 var Options = function (_React$Component4) {
   _inherits(Options, _React$Component4);
 
-  function Options() {
+  function Options(props) {
     _classCallCheck(this, Options);
 
-    return _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).apply(this, arguments));
+    var _this4 = _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).call(this, props));
+
+    _this4.handleRemoveAll = _this4.handleRemoveAll.bind(_this4);
+    return _this4;
   }
 
   _createClass(Options, [{
-    key: 'removeAll',
-    value: function removeAll() {
-      alert('remove all');
+    key: 'handleRemoveAll',
+    value: function handleRemoveAll() {
+      alert(this.props.options);
     }
   }, {
     key: 'render',
@@ -124,7 +127,7 @@ var Options = function (_React$Component4) {
         null,
         React.createElement(
           'button',
-          { onClick: this.removeAll },
+          { onClick: this.handleRemoveAll },
           'Remove all'
         ),
         this.props.options.map(function (option) {
