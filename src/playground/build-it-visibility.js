be50fcc166727bf29cc6fd56c37fb6fe77063dd3
Didac Esteban Andres <didacesteban@gmail.com>
2018-03-24 13:12:19 +0100

    added state to visibility example

--- src/playground/build-it-visibility.js
+++ src/playground/build-it-visibility.js
@@ -1,23 +1,31 @@
-let toggleVisible = false;
+class Template extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeToggleState = this.changeToggleState.bind(this);
+    this.state = {
+      toggleVisible: false
+    };
+  }
+  changeToggleState() {
+    this.setState((prevState) => {
+      return {
+        toggleVisible: prevState.toggleVisible == false ? true : false
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+          <button onClick={this.changeToggleState}>{this.state.toggleVisible == false ? 'Show details' : 'Hide details'}</button>
+          {this.state.toggleVisible && (
+            <div>
+            <p>Some details</p>
+            </div>
+          )}
+      </div>
+    )
+  }
+}
 
-const changeToggleState = () => {
-  toggleVisible = !toggleVisible;
-  renderApp();
-};
-
-const renderApp = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={changeToggleState}>{toggleVisible == false ? 'Show details' : 'Hide details'}</button>
-      {toggleVisible && (
-        <div>
-          <p>Some details</p>
-        </div>
-      )}
-    </div>
-  );
-  ReactDOM.render(template, document.getElementById('app'));
-};
-
-renderApp();
+ReactDOM.render(<Template />, document.getElementById('app'));
